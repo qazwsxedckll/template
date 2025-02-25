9299149fb90dcbf4ec6977183a8eedf75d7cd151
evilolipop <decai.wang@foxmail.com>
2025-02-25 15:31:38 +0800

    feat: release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,11 +13,12 @@ rm -rf release/*
 
 mkdir -p release/${module}
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    ditto configs release/${module}/configs
+    ditto configs/config.example.toml release/${module}
 else
     cp --parent configs/config.example.toml release/${module}
 fi
 cp README.md release/${module}
+cp Makefile release/${module}
 
 version=$(git describe --tags --abbrev=0 --always)
 go build -ldflags="-X ${module}/cmd.Version=${version}" -o release/${module}
